ef179f0a140ad5309e2f8b8ae3b5e02088313a11
Phill Holbrook <phill.holbrook@gmail.com>
2022-03-26 21:37:06 -0400

    Fixing missing Action import

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -1,4 +1,4 @@
-from argparse import ArgumentParser
+from argparse import Action, ArgumentParser
 
 class DriverAction(Action):
     def __call__(self, parser, namespace, values, option_string=None):
@@ -19,3 +19,17 @@ def create_parser():
             required = True)
 
     return parser
+
+def main():
+    import boto3
+    from pgbackup import pgdump, storage
+
+    args = create_parser().parse_args()
+    dump = pgdump.dump(args.url)
+    if args.driver == 's3':
+        client = boto3.client('s3')
+        storage.s3(client, dump.stdout, args.destination, 'example.sql')
+    else:
+        outfile = open(args.destination, 'wb')
+        storage.local(dump.stdout, outfile)
+
